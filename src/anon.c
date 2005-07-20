75e820d217124a258db5d638bd0fe13a79bcf5f6
Juergen Schoenwaelder <j.schoenwaelder@jacobs-university.de>
2005-07-20 19:24:09 +0000

    Use inet_ntop() and inet_pton(). Reduced some redundant code and added some more comments.

--- src/anon.c
+++ src/anon.c
@@ -14,22 +14,29 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 #include "libanon.h"
 
 static const char *progname = "anon";
 
-static void cmd_help(int argc, char **argv);
-static void cmd_ip(int argc, char **argv);
-static void cmd_ip_lex(int argc, char **argv);
+static void cmd_help(int argc, char **argv, int flags);
+static void cmd_ip(int argc, char **argv, int flags);
+
+#define ANON_FLAG_LEX	0x01
 
 static struct handler {
     const char *name;
-    void (*func)(int argc, char **argv);
+    void (*func)(int argc, char **argv, int);
+    int flags;
 } cmds[] = {
-    { "help",	cmd_help },
-    { "ip",	cmd_ip },
-    { "ip-lex",	cmd_ip_lex },
+    { "help",	cmd_help,	0 },
+    { "ip",	cmd_ip,		0 },
+    { "ip-lex",	cmd_ip,		ANON_FLAG_LEX },
     { NULL, NULL }
 };
 
@@ -38,6 +45,10 @@ static unsigned char my_key[32] =
   {21,34,23,141,51,164,207,128,19,10,91,22,73,144,125,16,
    216,152,143,131,121,121,101,39,98,87,76,45,42,132,34,2};
 
+/*
+ * Open a file and handle all errors by producing an error message
+ * before terminating the process.
+ */
 
 static FILE*
 xfopen(const char *filename, const char *mode)
@@ -53,100 +64,97 @@ xfopen(const char *filename, const char *mode)
     return f;
 }
 
+/*
+ * Trim a string be removing leading or trailing white space
+ * characters.
+ */
 
+static int
+trim(char *buffer)
+{
+    char *s, *e;
+
+    if (! buffer) return 0;
+
+    for (s = buffer; *s && isspace(*s); s++) ;
+
+    for (e = s + strlen(s)-1; e > s && isspace(*e); e--) *e = 0;
+    
+    memmove(buffer, s, e-s+2);
+    return 1;
+}
+
+/*
+ * Prefix preserving IP address anonymization.
+ */
 
 static void
 ip_pref(anon_ip_t *a, FILE *f)
 {
-    unsigned int raw_addr, anonymized_addr;
-    unsigned int packet_addr1, packet_addr2,
-	packet_addr3, packet_addr4;
+    in_addr_t raw_addr, anon_addr;
+    char buf[10*INET_ADDRSTRLEN];
 
     /*
-     * read ip addresses (one per input line) and print the anonymized
+     * read ip addresses (one per input line), call the prefix
+     * preserving anonymization function and print the anonymized
      * addresses
      */
 
-    while (fscanf(f, "%u.%u.%u.%u", &packet_addr1, &packet_addr2,
-		  &packet_addr3, &packet_addr4) == 4) {
-	/* convert the raw IP from a.b.c.d format into uint32_t format */
-	raw_addr = (packet_addr1 << 24) + (packet_addr2 << 16)
-	    + (packet_addr3 << 8) + packet_addr4;
+    while (fgets(buf, sizeof(buf), f) 
+	   && trim(buf)
+	   && inet_pton(AF_INET, buf, &raw_addr) > 0) {
+
+	anon_addr = anon_ip_map_pref(a, raw_addr);
 	
-	/* Anonymize the raw IP */
-	anonymized_addr = anon_ip_map_pref(a, raw_addr);
-	
-	/* convert the anonymized IP address from uint32_t 
-	 * to a.b.c.d format
-	 */
-	packet_addr1 = anonymized_addr >> 24;
-	packet_addr2 = (anonymized_addr << 8) >> 24;
-	packet_addr3 = (anonymized_addr << 16) >> 24;
-	packet_addr4 = (anonymized_addr << 24) >> 24;
-	
-	/* output the anonymized trace */
-	printf("%u.%u.%u.%u\n",  packet_addr1, packet_addr2,
-	       packet_addr3, packet_addr4 );
+	printf("%s\n", inet_ntop(AF_INET, &anon_addr, buf, sizeof(buf)));
     }
 }
 
+/*
+ * Prefix and lexicographic order preserving IP address anonymization.
+ */
 
 static void
 ip_lex(anon_ip_t *a, FILE *f)
 {
-    unsigned int raw_addr, anonymized_addr;
-    unsigned int packet_addr1, packet_addr2,
-	packet_addr3, packet_addr4;
+    in_addr_t raw_addr, anon_addr;
+    char buf[10*INET_ADDRSTRLEN];
 
     /*
      * first pass: read ip addresses (one per input line) and mark
      * them as used
      */
 
-    while (fscanf(f, "%u.%u.%u.%u", &packet_addr1, &packet_addr2,
-		  &packet_addr3, &packet_addr4) == 4) {
-	/* convert the raw IP from a.b.c.d format into uint32_t format */
-	raw_addr = (packet_addr1 << 24) + (packet_addr2 << 16)
-	    + (packet_addr3 << 8) + packet_addr4;
-	/* mark IP address and relevant nodes in the used_i tree */
+    while (fgets(buf, sizeof(buf), f) 
+	   && trim(buf)
+	   && inet_pton(AF_INET, buf, &raw_addr) > 0) {
+
 	anon_ip_set_used(a, raw_addr, 32);
     }
 
     /*
-     * second pass: read ip addresses and print the anonymized
-     * addresses
+     * second pass: read ip addresses (one per input line), call the
+     * prefix and lexcographic oder preserving anonymization function
+     * and print the anonymized addresses
      */
 
-    fseek(f,0,SEEK_SET);
-    while (fscanf(f, "%u.%u.%u.%u", &packet_addr1, &packet_addr2,
-		  &packet_addr3, &packet_addr4) == 4) {
-	/* convert the raw IP from a.b.c.d format into uint32_t format */
-	raw_addr = (packet_addr1 << 24) + (packet_addr2 << 16)
-	    + (packet_addr3 << 8) + packet_addr4;
+    rewind(f);
+    while (fgets(buf, sizeof(buf), f) 
+	   && trim(buf)
+	   && inet_pton(AF_INET, buf, &raw_addr) > 0) {
 	
-	/* Anonymize the raw IP */
-	anonymized_addr = anon_ip_map_pref_lex(a, raw_addr);
+	anon_addr = anon_ip_map_pref_lex(a, raw_addr);
 
-	/* convert the anonymized IP address from uint32_t 
-	 * to a.b.c.d format */
-
-	packet_addr1 = anonymized_addr >> 24;
-	packet_addr2 = (anonymized_addr << 8) >> 24;
-	packet_addr3 = (anonymized_addr << 16) >> 24;
-	packet_addr4 = (anonymized_addr << 24) >> 24;
-	
-	/* output the anonymized trace */
-	printf("%u.%u.%u.%u\n",  packet_addr1, packet_addr2,
-	       packet_addr3, packet_addr4 );
+	printf("%s\n", inet_ntop(AF_INET, &anon_addr, buf, sizeof(buf)));
     }
 }
 
 /*
- * Prefix-preserving IP address anonymization subcommand.
+ * Implementation of the 'anon ip' and 'anon ip-lex' commands.
  */
 
 static void
-cmd_ip(int argc, char **argv)
+cmd_ip(int argc, char **argv, int flags)
 {
     FILE *in;
     anon_ip_t *a;
@@ -164,48 +172,22 @@ cmd_ip(int argc, char **argv)
 	exit(EXIT_FAILURE);
     }
     anon_ip_set_key(a, my_key);
-    ip_pref(a, in);
+    if (flags & ANON_FLAG_LEX) {
+      ip_lex(a, in);
+    } else {
+      ip_pref(a, in);
+    }
     anon_ip_delete(a);
 
     fclose(in);
 }
 
 /*
- * Prefix-preserving and lexicographic-order preserving IP address
- * anonymization subcommand.
+ * Implementation of the 'anon help' command.
  */
 
 static void
-cmd_ip_lex(int argc, char **argv)
-{
-    FILE *in;
-    anon_ip_t *a;
-
-    if (argc != 3) {
-	fprintf(stderr, "%s: Too few arguments\n", progname);
-	exit(EXIT_FAILURE);
-    }
-
-    in = xfopen(argv[2], "r");
-
-    a = anon_ip_new();
-    if (! a) {
-	fprintf(stderr, "%s: Failed to initialize IP mapping\n", progname);
-	exit(EXIT_FAILURE);
-    }
-    anon_ip_set_key(a, my_key); 
-    ip_lex(a, in);
-    anon_ip_delete(a);
-
-    fclose(in);
-}
-
-/*
- * Print some hopefully helpful usage information.
- */
-
-static void
-cmd_help(int argc, char **argv)
+cmd_help(int argc, char **argv, int ignored)
 {
     int i;
 
@@ -247,7 +229,9 @@ int main(int argc, char * argv[]) {
 	return EXIT_FAILURE;
     }
 
-    (cmds[i].func) (argc, argv);
+    if (cmds[i].func) {
+	(cmds[i].func) (argc, argv, cmds[i].flags);
+    }
 
     return EXIT_SUCCESS;
 }
