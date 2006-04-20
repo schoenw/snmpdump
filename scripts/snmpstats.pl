aaa9eb83dd2e3bbf4dd97f6aa7e116bc213cfa31
Juergen Schoenwaelder <j.schoenwaelder@jacobs-university.de>
2006-04-20 09:10:12 +0000

    Added code to compute the distribution of the number of varbind per operation and the status code distribution per operation.

--- scripts/snmpstats.pl
+++ scripts/snmpstats.pl
@@ -14,19 +14,34 @@ use strict;
 
 my @basic_vers;
 my %basic_ops;
+my %basic_errs;
+my %basic_errs_max;
+my %basic_nvbs;
+my %basic_nvbs_max;
 
 sub basic {
     my $aref = shift;
     my $version = ${$aref}[6];
     my $op = ${$aref}[7];
+    my $err = ${$aref}[9];
+    if ($err > $basic_errs_max{$op}) {
+	$basic_errs_max{$op} = $err;
+    }
+    my $nvbs = ${$aref}[11];
+    if ($nvbs > $basic_nvbs_max{$op}) {
+	$basic_nvbs_max{$op} = $nvbs;
+    }
     $basic_vers[$version]++;
     $basic_ops{"$version,$op"}++;
+    $basic_errs{"$op,$err"}++;
+    $basic_nvbs{"$op,$nvbs"}++;
 }
 
 sub basic_done {
     my $total = shift;
     printf("# The following table shows the protocol operations statistics\n" .
-	   "# broken down by SNMP protocol version plus the overall sums\n\n");
+	   "# broken down by SNMP protocol version plus the overall sums.\n" .
+	   "\n");
     printf("%-18s  %12s  %12s  %12s  %12s\n", 
 	   "OPERATION", "SNMPv1", "SNMPv2c", "SNMPv3", "TOTAL");
     foreach my $op ("get-request", "get-next-request", "get-bulk-request",
@@ -49,6 +64,42 @@ sub basic_done {
 	$sum += $basic_vers[$version];
     }
     printf(" %8d %3d\%\n", $sum, $sum*100/$total);
+    
+    printf("\n" .
+	   "# The following table shows the distribution of the number of\n" .
+	   "# elements in the varbind list broken down by operation type.\n" .
+	   "\n");
+    printf("%-18s  %12s  %15s\n", 
+	   "OPERATION", "VARBINDS", "NUMBER");
+    foreach my $op ("get-request", "get-next-request", "get-bulk-request",
+		    "set-request", "trap", "trap2", "inform",
+		    "response", "report") {
+	for (my $i = 0; $i <= $basic_nvbs_max{$op}; $i++) {
+	    if ($basic_nvbs{"$op,$i"}) {
+		printf("%-18s  %12d %12d %3d\%\n", "$op:", $i, 
+		       $basic_nvbs{"$op,$i"},
+		       $basic_nvbs{"$op,$i"}*100/$total);
+	    }
+	}
+    }
+
+    printf("\n" .
+	   "# The following table shows the distribution of the status\n" .
+	   "# codes broken down by operation type.\n" .
+	   "\n");
+    printf("%-18s  %12s  %15s\n", 
+	   "OPERATION", "STATUS", "NUMBER");
+    foreach my $op ("get-request", "get-next-request", "get-bulk-request",
+		    "set-request", "trap", "trap2", "inform",
+		    "response", "report") {
+	for (my $i = 0; $i <= $basic_errs_max{$op}; $i++) {
+	    if ($basic_errs{"$op,$i"}) {
+		printf("%-18s  %12d %12d %3d\%\n", "$op:", $i, 
+		       $basic_errs{"$op,$i"},
+		       $basic_errs{"$op,$i"}*100/$total);
+	    }
+	}
+    }
 }
 
 sub process {
