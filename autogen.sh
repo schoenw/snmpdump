952708e57e22c4f792f9df2ee714fd936f51dbfa
Juergen Schoenwaelder <j.schoenwaelder@jacobs-university.de>
2006-09-06 13:11:43 +0000

    Added a patch originally submitted to libsmi to find glibtool and glibtoolize on MacOS darwinports installations.

--- autogen.sh
+++ autogen.sh
@@ -2,11 +2,21 @@
 #
 # Run this to generate all the initial makefiles.
 #
-# $Id: autogen.sh 1148 2001-08-16 15:28:30Z strauss $
+# $Id$
 
 DIE=true
 PROJECT="snmpdump"
 
+case `uname -s` in
+    Darwin)
+       libtool=glibtool
+       libtoolize=glibtoolize
+       ;;
+    *)
+       libtool=libtool
+       libtoolize=libtoolize
+       ;;
+esac
 
 VER=`bison --version | sed 's/^[a-zA-Z ]*//'`
 case "$VER" in
@@ -66,7 +76,7 @@ _EOF_
 esac
 
 
-VER=`libtool --version | grep ' libtool)' | \
+VER=`${libtool} --version | grep ' libtool)' | \
 sed 's/.*) \([0-9][0-9.]*\) .*/\1/' `
 case "$VER" in
 0* | 1\.[0-2] | 1\.[0-2][a-z]* | \
@@ -84,7 +94,7 @@ esac
 
 $DIE
 
-libtoolize --copy --force || exit 1
+${libtoolize} --copy --force || exit 1
 
 if test -z "$*"; then
 	echo "Running ./configure with no arguments. If you wish to pass any,"
